a120205a8a8e31966348932a6b30598a6da497a5
Giray Aksakal <girayaksakal@icloud.com>
2024-05-08 21:47:28 +0300

    Initial Commit

---
+++ appcentNews/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  appcentNews
+//
+//  Created by Giray Aksakal on 8.05.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ appcentNews/appcentNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  appcentNewsApp.swift
+//  appcentNews
+//
+//  Created by Giray Aksakal on 8.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct appcentNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
